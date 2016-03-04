36a3669f395c24e73ef579997f39bc826ecf577e
Peter Liu <Peterljr888@gmail.com>
2016-03-04 06:31:38 -0500

    fix axis comparator

--- js/app.js
+++ js/app.js
@@ -469,8 +469,8 @@
                 leftEquiv: floatParse(srch.leftEquiv)
             };
 
-            var axisMinFunc = function(axis) { return (axis < 15) ? 0 : (axis < 165) ? axis-15 : 165; };
-            var axisMaxFunc = function(axis) { return (axis > 165) ? 180 : (axis > 15) ? axis+15 : 15; };
+            var axisMinFunc = function(axis) { return (axis <= 15) ? 0 : (axis < 165) ? axis-15 : 165; };
+            var axisMaxFunc = function(axis) { return (axis >= 165) ? 180 : (axis > 15) ? axis+15 : 15; };
             var searchObjFunc = function(equiv, cyl, axis) {
                 return {
                     equivMin: equiv,
